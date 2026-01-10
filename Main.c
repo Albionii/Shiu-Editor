740ff141e08be879314614b985d756b07d6f6867
Albion Qerreti <albionqerreti221b@gmail.com>
2026-01-10 14:56:31 +0100

    Fixed double flickering of window

--- Main.c
+++ Main.c
@@ -3,62 +3,71 @@
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <wchar.h>
 
-struct Shiu {
+#define WINDOW_TITLE "Shiu Editor"
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
+
+struct Editor {
     SDL_Window *window;
     SDL_Renderer *renderer;
 };
 
-bool sdl_initialize(struct Shiu *shiu);
+bool sdl_initialize(struct Editor *editor);
+void editor_cleanup(struct Editor *editor);
 
 int main() {
-    // TTF_Init();
+    struct Editor editor = {
+        .window = NULL,
+        .renderer = NULL
+    };
     
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        printf("SDL_Init error: %s\n", SDL_GetError());
-        return 1;
-    }
+    if(sdl_initialize(&editor)){
+        editor_cleanup(&editor);
+        exit(0);
+    };
 
-    SDL_Window *window = SDL_CreateWindow(
-        "Shiu Editor",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        500,
-        500,
-        SDL_WINDOW_SHOWN
-    );
 
-    if (!window) {
-        printf("Window error: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    
+    editor_cleanup(&editor);
+    return 0;
+}
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(
-        window, -1, SDL_RENDERER_ACCELERATED
-    );
-
-    if (!renderer) {
-        printf("Renderer error: %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    int running = 1;
-    SDL_Event e;
-
-    while (running) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_QUIT) {
-                running = 0;
-            } 
-        }
-    }
-    SDL_StopTextInput();
-
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+void editor_cleanup(struct Editor *editor){
+    SDL_DestroyRenderer(editor -> renderer);
+    SDL_DestroyWindow(editor -> window);
     SDL_Quit();
+}
+
+bool sdl_initialize(struct Editor *editor){
+    if (SDL_Init(SDL_INIT_EVERYTHING)) {
+        fprintf(stderr, "SDL_Init error: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    editor->window = SDL_CreateWindow(
+        WINDOW_TITLE,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREEN_WIDTH,
+        SCREEN_HEIGHT,
+        SDL_WINDOW_HIDDEN
+    );
+    
+    if (!editor -> window){
+        fprintf(stderr, "Error creating window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    editor -> renderer = SDL_CreateRenderer(editor->window, -1, 0);
+    
+    if (!editor -> renderer){
+        fprintf(stderr, "Error creating renderer: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    SDL_ShowWindow(editor->window);
+    
     return 0;
 }
