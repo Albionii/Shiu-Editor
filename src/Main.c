d3b763061064e2aff240501d124702f5f11fa316
Albion Qerreti <albionqerreti221b@gmail.com>
2026-01-24 20:07:14 +0100

    Implemented the blinking cursor

--- src/Main.c
+++ src/Main.c
@@ -32,7 +32,9 @@ struct Editor {
     TTF_Font *font;
     SDL_Texture *text_texture;
     SDL_Rect text_rect;
-    
+    int char_w;
+    int char_h;
+
     GapBuffer *gb;
 };
 
@@ -56,7 +58,7 @@ int main() {
     
     //Here is the main editor logic
 
-    window_customize(&editor);
+    // window_customize(&editor);
     
     SDL_Event event;
 
@@ -67,10 +69,10 @@ int main() {
     while (true) {
         while (SDL_PollEvent(&event)) {
             switch(event.type){
-                case SDL_QUIT: 
+                case SDL_QUIT:
                     editor_cleanup(&editor, EXIT_SUCCESS);
                     break;
-                case SDL_TEXTINPUT: 
+                case SDL_TEXTINPUT:
                     update_text_texture(&editor, event.text.text);
                     break;
                 case SDL_KEYDOWN:
@@ -82,14 +84,14 @@ int main() {
                     break;
             }
         }
-        
-        
+
+
         SDL_SetRenderDrawColor(editor.renderer, 30, 30, 30, 255);
         SDL_RenderClear(editor.renderer);
         SDL_RenderCopy(editor.renderer, editor.text_texture, NULL, &editor.text_rect);
         draw_cursor(&editor);
         SDL_RenderPresent(editor.renderer);
-        
+
         //Delay the loop to be around 60 frames/s
         SDL_Delay(16);
     }
@@ -136,16 +138,17 @@ bool sdl_initialize(struct Editor *editor){
     }
 
     SDL_ShowWindow(editor->window);
-    
+
     TTF_Init();
-    
-    //Font Local to my Linux machine.
-    editor->font = TTF_OpenFont("/usr/share/fonts/X11/Type1/C059-Italic.pfb", 24);
+
+    //Font Monospace Local to my Linux machine.
+    editor->font = TTF_OpenFont("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", 24);
     if (!editor->font) {
         fprintf(stderr, "Font load error: %s\n", TTF_GetError());
         return 1;
     }
-    
+
+    TTF_SizeUTF8(editor->font, "B", &editor->char_w, &editor->char_h);
     SDL_StartTextInput();
     return 0;
 }
@@ -161,12 +164,14 @@ void handle_key_presses(struct Editor *editor, SDL_Event *event){
             size_t gl = editor->gb->gap_left;
             while (gl > 0 && editor->gb->buffer[gl-1] != '\n') gl--;
             if(gl == 0) break;
-           
+
             gl--;
-            
             gb_move_cursor(editor->gb, gl);
             break;
         }
+        case SDLK_DOWN:
+
+            break;
         case SDLK_LEFT:
             if (editor->gb->gap_left > 0) {
                 gb_move_cursor(editor->gb, editor->gb->gap_left - 1);
@@ -179,24 +184,19 @@ void handle_key_presses(struct Editor *editor, SDL_Event *event){
             }
             break;
         }
-        case SDLK_DOWN:
-            printf("BUFFER SIZE : %ld \n", editor->gb->capacity);
-            printf("GAP LEFT : %ld \n", editor->gb->gap_left);
-            printf("GAP RIGHT : %ld \n", editor->gb->gap_right);
-            break;
         case SDLK_KP_ENTER:
-        case SDLK_RETURN: 
+        case SDLK_RETURN:
             gb_insert(editor->gb, "\n");
             break;
         case SDLK_BACKSPACE:
             gb_backspace(editor->gb);
-            break;     
+            break;
         //Save the file Ctrl + s
-        case SDLK_s: 
+        case SDLK_s:
             if(event->key.keysym.mod & SDLK_LCTRL){
                 save_to_file(editor, "jamesbond.c");
             }
-            
+
             //TODO:Maybe should add Save as to differ from Save
             break;
         default:
@@ -210,21 +210,21 @@ void update_text_texture(struct Editor *editor, const char *new_text) {
     if (new_text){
         gb_insert(editor->gb, new_text);
     }
-    
+
     char *text_to_render = gb_get_string(editor -> gb);
 
     if (editor->text_texture) {
         SDL_DestroyTexture(editor->text_texture);
     }
 
-    
+
     SDL_Color white = {255, 255, 255, 255};
     SDL_Surface *surface = TTF_RenderUTF8_Blended_Wrapped(
         editor->font,
         text_to_render,
         white,
         WRAP_TEXT_WIDTH);
-    
+
     if (surface) {
         editor->text_texture = SDL_CreateTextureFromSurface(editor->renderer, surface);
         editor->text_rect.w = surface->w;
@@ -239,7 +239,29 @@ void update_text_texture(struct Editor *editor, const char *new_text) {
  * Draws the cursor where the next character will be written.
  */
 void draw_cursor(struct Editor *editor) {
-    
+    if ((SDL_GetTicks() / 500) % 2 != 0) return;
+
+    int row = 0;
+    int col = 0;
+
+    for (size_t i = 0; i < editor->gb->gap_left; i++){
+        if (editor->gb->buffer[i] == '\n'){
+            row++;
+            col = 0;
+        }else {
+            col++;
+        }
+    }
+
+    SDL_Rect cursor_rect = {
+        editor->text_rect.x + (col * editor->char_w),
+        editor->text_rect.y + (row * editor->char_h),
+        2,
+        TTF_FontHeight(editor->font)
+    };
+    SDL_SetRenderDrawColor(editor->renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(editor->renderer, &cursor_rect);
+
 }
 
 
@@ -249,10 +271,10 @@ void filename_popup(struct Editor *editor){
         printf("ERROR: Could not open zenity %p : ", file);
         return;
     }
-    
+
     char path[100];
     fclose(file);
-}   
+}
 
 
 /**
@@ -260,22 +282,21 @@ void filename_popup(struct Editor *editor){
  */
 void save_to_file(struct Editor *editor, char *filename){
     // filename_popup(editor);
-    FILE *file = fopen(filename, "w");  
+    FILE *file = fopen(filename, "w");
     if (!file){
         printf("ERROR: Could not open file %s for writing : ", filename);
         return;
     }
-    
+
     if (editor->gb->gap_left > 0){
         fwrite(editor->gb->buffer, 1, editor->gb->gap_left, file);
     }
-    
+
     size_t right_side = editor->gb->gap_right + 1;
     size_t right_side_length = editor->gb->capacity - right_side;
-    
+
     if (right_side_length > 0) {
         fwrite(editor->gb->buffer + right_side, 1, right_side_length, file);
     }
     fclose(file);
 }
-
